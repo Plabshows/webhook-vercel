cb0ce8e88863a878268d186c5419e28a28d47067
Plabshows <performancelabshows@gmail.com>
2025-02-23 19:16:43 +0400

    Corrigiendo instalación de axios

--- api/webhook.js
+++ api/webhook.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 
+
 export default async function handler(req, res) {
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Método no permitido, usa POST" });
